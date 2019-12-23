27a0e7e177c35620d1c9da45f030ef0eae63c947
Chi Yeung <yeun0007@umn.edu>
2019-12-23 07:56:33 -0600

    Map Activity for Result

--- mobile/src/main/java/com/ctyeung/runyasso800/ResultActivity.kt
+++ mobile/src/main/java/com/ctyeung/runyasso800/ResultActivity.kt
@@ -1,23 +1,97 @@
 package com.ctyeung.runyasso800
 
+import android.app.Activity
+import android.content.pm.PackageManager
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.databinding.DataBindingUtil
-import com.ctyeung.runyasso800.databinding.ActivityResultBinding
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 
-/*
- * Review results after run !
- * - fragment1 : show map with GPS co-ordinates
- * - fragment2 : show recyclerview of splits
- */
-class ResultActivity : AppCompatActivity() {
-    lateinit var binding: ActivityResultBinding
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.Marker
+import com.google.android.gms.maps.model.MarkerOptions
+
+class ResultActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnMarkerClickListener {
+
+    private lateinit var activity: Activity
+    private lateinit var mMap: GoogleMap
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_result)
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        val mapFragment = supportFragmentManager
+            .findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+        activity = this
+        askPermissions()
+    }
 
-        binding = DataBindingUtil.setContentView(this, R.layout.activity_result)
-        binding?.listener = this
+    companion object {
+        private const val LOCATION_PERMISSION_REQUEST_CODE = 1
+    }
+
+    protected fun shouldAskPermissions(): Boolean {
+        return Build.VERSION.SDK_INT > Build.VERSION_CODES.LOLLIPOP_MR1
+    }
+
+    protected fun askPermissions() {
+        val permissions = arrayOf(
+            "android.permission.ACCESS_FINE_LOCATION",
+            "android.permission.ACCESS_COARSE_LOCATION"
+        )
+        val requestCode = 200
+        requestPermissions(permissions, requestCode)
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int,
+                                            permissions: Array<String>,
+                                            grantResults: IntArray)
+    {
+        for(permission:String in permissions)
+        {
+            // result:0 is ok
+            val result = ContextCompat.checkSelfPermission(activity, permission)
+            if (0!=result)
+            {
+                // not permitted to save or read -- !!! data-binding refactor
+                return
+            }
+        }
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+
+        /*
+         * load markers from splits
+         */
+
+        mMap = googleMap
+        mMap.getUiSettings().setZoomControlsEnabled(true)
+        mMap.setOnMarkerClickListener(this)
+
+        val myPlace = LatLng(40.73, -73.99)  // this is New York
+        mMap.addMarker(MarkerOptions().position(myPlace).title("My Favorite City"))
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(myPlace, 12.0f))
+        setUpMap()
+    }
+
+    private fun setUpMap() {
+        if (ActivityCompat.checkSelfPermission(this,
+                android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION), LOCATION_PERMISSION_REQUEST_CODE)
+            return
+        }
+    }
+
+    override fun onMarkerClick(p0: Marker?): Boolean {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return false
     }
 }
