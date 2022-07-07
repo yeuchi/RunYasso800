e3c2708ff451633408a636f794ec3b939f661faf
Chi Yeung <yeun0007@umn.edu>
2022-07-06 20:37:20 -0500

    work in progress : Goal
    Re-implemented Goal - no real change.

--- app/src/main/java/com/ctyeung/runyasso800/features/goals/GoalActivity.kt
+++ app/src/main/java/com/ctyeung/runyasso800/features/goals/GoalActivity.kt
@@ -1,13 +1,18 @@
 package com.ctyeung.runyasso800.features.goals
 
+import android.app.TimePickerDialog
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.widget.Toast
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
 import com.ctyeung.runyasso800.BaseActivity
 import com.ctyeung.runyasso800.R
 import com.ctyeung.runyasso800.databinding.ActivityGoalBinding
 import com.ctyeung.runyasso800.features.run.RunActivity
 import com.ctyeung.runyasso800.storage.SharedPrefUtility
+import java.util.*
 
 class GoalActivity : BaseActivity() {
     lateinit var binding:ActivityGoalBinding
@@ -35,13 +40,67 @@ class GoalActivity : BaseActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_goal)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_goal)
+        binding.goal = this
+
+        model = ViewModelProvider(this).get(GoalViewModel::class.java)
+        model.setInitValues()
+        handleTextChange()
+    }
+
+    /*
+     * Training Event name
+     */
+    fun handleTextChange() {
+        binding.txtRunName.addTextChangedListener(object : TextWatcher {
+
+            override fun afterTextChanged(s: Editable)
+            {
+                var name:String? = s.toString()
+                if(null==name || 0==name.length) {
+                    // use default if invalid
+                    name = resources.getString(R.string.run_yasso_800)
+                }
+
+                model.persistName(name!!)
+                initActionBar()
+
+                // go to next activity automatically
+                //if (hasName && hasRaceGoal)
+                //    onClickNext()
+            }
+            override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {}
+            override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {}
+        })
     }
 
     /*
     * Select Race Goal time in hours and minutes
     */
     fun onClickTime()
-    {}
+    {
+        // Get Current time
+        val c = Calendar.getInstance()
+        val hour = c.get(Calendar.HOUR_OF_DAY)
+        val minute = c.get(Calendar.MINUTE)
+
+        val timePickerDialog = TimePickerDialog(this@GoalActivity,
+            TimePickerDialog.OnTimeSetListener{ view, hourOfDay, minute ->
+                model.setRaceGoal(hourOfDay, minute)
+                model.setSprintGoal(hourOfDay, minute)
+                binding.invalidateAll()
+                initActionBar()
+
+                if(0==hourOfDay)
+                    Toast.makeText(this, resources.getString(R.string.goal_unreal), Toast.LENGTH_LONG).show()
+
+                // go to next activity automatically
+                //if(hasName && hasRaceGoal)
+                //    onClickNext()
+
+            }, hour, minute, true
+        )
+        timePickerDialog.show()
+    }
 
     /*
     * Next -> RunActivity
--- app/src/main/java/com/ctyeung/runyasso800/features/goals/GoalViewModel.kt
+++ app/src/main/java/com/ctyeung/runyasso800/features/goals/GoalViewModel.kt
@@ -2,9 +2,40 @@ package com.ctyeung.runyasso800.features.goals
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
+import com.ctyeung.runyasso800.MainApplication
+import com.ctyeung.runyasso800.R
+import com.ctyeung.runyasso800.storage.SharedPrefUtility
+import com.ctyeung.runyasso800.storage.TimeFormatter
 
 class GoalViewModel (application: Application) : AndroidViewModel(application){
     var name:String = ""
     var sprintGoal:String = ""
     var raceGoal:String = ""
+
+    fun setInitValues(){
+        name = SharedPrefUtility.get(SharedPrefUtility.keyName, MainApplication.applicationContext().resources.getString(
+            R.string.run_yasso_800))
+        val sprintInSeconds:Long = SharedPrefUtility.get(SharedPrefUtility.keySprintGoal, 0L)
+        sprintGoal = TimeFormatter.printTime(sprintInSeconds)
+
+        val raceInSeconds:Long = SharedPrefUtility.get(SharedPrefUtility.keyRaceGoal, 0L)
+        raceGoal = TimeFormatter.printTime(raceInSeconds)
+    }
+
+    fun persistName(str:String) {
+        SharedPrefUtility.set(SharedPrefUtility.keyName, str)
+        name = str
+    }
+
+    fun setSprintGoal(hourOfDay:Int, minute:Int) {
+        val sprintInSeconds = TimeFormatter.convertHHmmss(0, hourOfDay, minute)
+        SharedPrefUtility.set(SharedPrefUtility.keySprintGoal, sprintInSeconds)
+        sprintGoal = TimeFormatter.printTime(sprintInSeconds)
+    }
+
+    fun setRaceGoal(hourOfDay:Int, minute:Int) {
+        val raceInSeconds = TimeFormatter.convertHHmmss(hourOfDay, minute, 0)
+        SharedPrefUtility.set(SharedPrefUtility.keyRaceGoal, raceInSeconds)
+        raceGoal = TimeFormatter.printTime(raceInSeconds)
+    }
 }
