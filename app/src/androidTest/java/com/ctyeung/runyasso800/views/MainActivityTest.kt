c2b3feeff967c6560448c76a6f888d2571d6592e
Chi Yeung <yeun0007@umn.edu>
2022-10-16 19:04:59 -0500

    Test runner ok
    1. Test runner ok (not breaking app execution).
    2. Test running ok.
    3. App running ok.

--- app/src/androidTest/java/com/ctyeung/runyasso800/views/MainActivityTest.kt
+++ app/src/androidTest/java/com/ctyeung/runyasso800/views/MainActivityTest.kt
@@ -1,10 +1,30 @@
 package com.ctyeung.runyasso800.views
 
+import dagger.hilt.android.testing.HiltAndroidRule
+import dagger.hilt.android.testing.HiltAndroidTest
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+
 /*
  * Reference:
  * Coding with Mitch
  * https://www.youtube.com/watch?v=vVJeOACGSOU
  */
-class MainActivityTest {
 
+@HiltAndroidTest
+
+class MainActivityTest {
+    @get:Rule(order = 0)
+    var hiltRule = HiltAndroidRule(this)
+
+    @Before
+    fun init() {
+        hiltRule.inject()
+    }
+
+    @Test
+    fun happy_path_goal() {
+
+    }
 }
